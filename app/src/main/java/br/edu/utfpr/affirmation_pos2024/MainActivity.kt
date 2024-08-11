548832da26b563085e64fb9fa1fa68ffa8af06ab
HenriqueAndrew <henriqueandrew1986_@hotmail.com>
2024-08-11 12:29:07 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/affirmation_pos2024/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.edu.utfpr.affirmation_pos2024
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.utfpr.affirmation_pos2024.adapter.ItemAdapter
+import br.edu.utfpr.affirmation_pos2024.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>( R.id.recycler_view )
+        val myDataset = Datasource().loadAffirmation()
+
+        recyclerView.adapter = ItemAdapter( this, myDataset )
+
+        recyclerView.setHasFixedSize( true )
+
+    }
+}
---
+++ app/src/main/java/br/edu/utfpr/affirmation_pos2024/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package br.edu.utfpr.affirmation_pos2024.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation (@StringRes var stringResourceId: Int, @DrawableRes var imageResourceId: Int)
